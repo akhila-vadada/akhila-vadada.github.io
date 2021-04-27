e08381b7dd07d791dc2cc22b696bcb56e11b426c
akhila-vadada <71364717+akhila-vadada@users.noreply.github.com>
2021-04-27 13:27:12 +0530

    Update script.js

--- AssignmentWeek4/script.js
+++ AssignmentWeek4/script.js
@@ -1,13 +1,13 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
+names[0]="Akhila";
+names[1]="Jaanu";
+names[2]="Sahithi";
+names[3]="jimin";
+names[4]="Taehyung";
+names[5]="Jungkook";
+names[6]="Namjoon";
+names[7]="seojoon";
+names[8]="Krithika";
 names[9]="jim";
 
 
